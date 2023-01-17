d3a79181744097844e4b4c541994994dfa727674
Start-Select <nielsdelaat04@gmail.com>
2023-01-17 10:04:37 +0100

    draw.html now shows prompt from prompt.html

--- prompt.js
+++ prompt.js
@@ -1,3 +1,10 @@
-function prompt(){
-    window.alert("JAVASCRIPT");
+function prompt(id){ 
+    if (id == 10) {
+        var prompt = sessionStorage.getItem("chosen");
+        document.getElementById(id).innerHTML = prompt;     
+    }
+    if  (id != 10) {
+    var prompt = document.getElementById(id).innerHTML;
+    sessionStorage.setItem("chosen", prompt);
+    }
 }
