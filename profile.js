0134e2830faea953717b6e5f12fa1cffcc504da1
Start-Select <nielsdelaat04@gmail.com>
2023-01-19 15:32:54 +0100

    Started profile page -back button -profile info -buttons

---
+++ profile.js
@@ -0,0 +1,9 @@
+function art() {
+    document.getElementById("art-bt").style.backgroundColor = '#5375E6';
+    document.getElementById("tags-bt").style.backgroundColor = '#E34BF2';
+}
+
+function tags() {
+    document.getElementById("tags-bt").style.backgroundColor = '#5375E6';
+    document.getElementById("art-bt").style.backgroundColor = '#E34BF2';
+}
